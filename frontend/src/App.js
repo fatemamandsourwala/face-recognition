36fe5477ecf3aa9f7b3c2730a9c96cdcc2954211
Fatema Mandsourwala <f.mandsourwala4993@gmail.com>
2020-06-17 10:28:55 +1000

    chore: update server host

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		fetch('http://localhost:3000/imageapi', {
+		fetch('https://fatema-smart-brain-app.herokuapp.com/imageapi', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
@@ -90,7 +90,7 @@ class App extends Component {
 			.then(response => response.json())
 			.then(response => {
 				if (response) {
-					fetch('http://localhost:3000/image', {
+					fetch('https://fatema-smart-brain-app.herokuapp.com/image', {
 						method: 'put',
 						headers: { 'Content-Type': 'application/json' },
 						body: JSON.stringify({
