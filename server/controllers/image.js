8924719b72a208ed6f34338588324d01680d6ba3
Fatema Mandsourwala <f.mandsourwala4993@gmail.com>
2020-06-18 12:19:08 +1000

    chore: read clarifai api key from env variable

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-	apiKey: '##########################',
+	apiKey: process.env.CLARIFAI_API_KEY,
 });
 
 const handleImageApi = (req, res) => {
